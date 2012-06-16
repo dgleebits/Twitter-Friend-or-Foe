901e8729a2287fd6167ef6e009f35a9c4d36457e
Dan Gleebits <dangleebits@hotmail.ca>
2012-06-16 15:09:08 -0700

    Update master

--- tweetClean.py
+++ tweetClean.py
@@ -1,5 +1,26 @@
+# python program to help clean up tweets
+# file loads, data cleans, and words counted
+
 data = open('TweetBackup15June2012').read().lower()
 
+data = data.replace('''!''',' ')
+data = data.replace('''$''',' ')
+data = data.replace('''%''',' ')
+data = data.replace('''^''',' ')
+data = data.replace('''&''',' ')
+data = data.replace('''*''',' ')
+data = data.replace(''':''',' ')
+data = data.replace(''';''',' ')
+data = data.replace('''<''',' ')
+data = data.replace('''>''',' ')
+data = data.replace('''{''',' ')
+data = data.replace('''}''',' ')
+data = data.replace('''[''',' ')
+data = data.replace(''']''',' ')
+data = data.replace('''|''',' ')
+data = data.replace('''-''',' ')
+data = data.replace('''+''',' ')
+data = data.replace('''=''',' ')
 data = data.replace('''http://t.co/''',' ')
 data = data.replace('''\n''',' ')
 data = data.replace('''RT''',' ')
@@ -17,6 +38,8 @@ data = data.replace('''jan''',' ')
 data = data.replace('''feb''',' ')
 data = data.replace('''mar''',' ')
 data = data.replace('''may''',' ')
+data = data.replace('''/''',' ')
+data = data.replace('''~''',' ')
 data = data.replace('''jun''',' ')
 data = data.replace('''jul''',' ')
 data = data.replace('''aug''',' ')
@@ -24,3 +47,16 @@ data = data.replace('''sep''',' ')
 data = data.replace('''oct''',' ')
 data = data.replace('''nov''',' ')
 data = data.replace('''dec''',' ')
+data = data.replace('''\x9c''',' ')
+data = data.replace('''\xe2''',' ')
+data = data.replace('''\xa2''',' ')
+data = data.replace('''\xe3''',' ')
+
+countList={}
+data = data.split()
+for word in data:
+  if word in countList :
+		countList[word] = countList[word]+1
+	else :
+		countList[word] = 1
+
